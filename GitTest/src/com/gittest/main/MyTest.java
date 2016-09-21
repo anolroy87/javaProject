c004a6b60c7ab1be6fb79cf8fa063babceb181e9
anolroy <anolroy@us.ibm.com>
2016-09-21 19:20:58 +0530

    initial GitTest java project

---
+++ GitTest/src/com/gittest/main/MyTest.java
@@ -0,0 +1,12 @@
+package com.gittest.main;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+
+	}
+
+}
