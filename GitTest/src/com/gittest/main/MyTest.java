370575d5ac661af0cb83242c02b7a5eb6dff78ae
anolroy <anolroy@us.ibm.com>
2016-09-29 18:41:06 +0530

    3rd Update to practice jenkins Build from git commit

--- GitTest/src/com/gittest/main/MyTest.java
+++ GitTest/src/com/gittest/main/MyTest.java
@@ -12,7 +12,7 @@ public class MyTest {
 		
 		System.out.println("This is an update that is done in Updates branch locally\n");
 
-		System.out.println("Change to Test automated Jenkins Build on GIT COMMIT!!");
+		System.out.println("Change to test automated Jenkins Build on GIT COMMIT!!");
 		
 		// ToDO:  ADD more classes in the project , in update branch. Then do selective Merge, and Push
 		
